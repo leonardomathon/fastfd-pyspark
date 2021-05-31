415f77dc8cf73d363f2d5f7e7843ce7956bdd12a
leonardomathon <leonardo.mathon@hotmail.com>
2021-05-31 18:52:17 +0200

    Pseudocode

--- src/FastFD.py
+++ src/FastFD.py
@@ -4,3 +4,18 @@ class FastFD:
 
     def print_dataset(self):
         print(dataset)
+
+    def gen_diff_sets(self, R, r):
+        # TODO
+
+    def execute(self):
+        # gen_diff_sets(R, r)
+
+        # For each column in R:
+            # Compute D^A_r from D^r
+
+            # If D^A_r == empty:
+                # Output "empty --> A"
+            
+            # Else if empty \notin D^A_r then:
+                # find_covers(...)
